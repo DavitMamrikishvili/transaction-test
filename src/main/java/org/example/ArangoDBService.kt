5feb20835d49a10082a6653599db14ffe2935f11
DavitMamrikishvili <davit.mamrikishvili1@gmail.com>
2023-01-18 03:09:52 -0800

    Update

--- src/main/java/org/example/ArangoDBService.kt
+++ src/main/java/org/example/ArangoDBService.kt
@@ -14,19 +14,23 @@ class ArangoDBService(appCtx: Context) {
     private val config = appCtx.configuration
     private val arango: Arango = Arango(config.arangoCredentials)
     private val db = arango.getDatabase()
-    private val events = (1..200_000).map {
+    private val events1 = (1..100_000).map {
         Event("862a3cb7-79f7-4f33-93ee-8797bee7e439:3fad95f4-3881-11ec-8760-$it",  "book", "scope","862a3cb7-79f7-4f33-93ee", "862a3cb7-79f7-4f33-93ee-8797bee7e439", false, "name", "type", 1751358236167872, 1751358236167879, "862a3cb7-79f7-4f33-93ee-8797bee7e439:3fad47d2-3881-11ec-8760-e55c32c0bc13", true, setOf(),
         "xwewsecchmtibgnkiwdfutitniyjltabnpawbkxpuebicuprioefuglxxkrtwjhxgvtadqmpfnxtvegevcyerixjqntqntqqahlytxuaavtinoajlqrejzcvqoswytprimcepebqrbjpabrxqzbwwzlojvlmztexeijyujwkmixqeleyaqniortbwdgfxecpvaqbndajcrjyhrazyojjployqybhrtghimiimpqpplrsmufxicnvfnpanlpntxilchfrezgeqtwqgfisrnxpatjsmvizwrayeapifnabvvejeuqspoifnvmlscapuxzdpccwueptarinrkwnpfgcpxsjhbxkmrvlarepjentaozfdzhjadojdukyphraugginbpqnnwlxooozyxokvnowaoicangcuegvgngiwvzaudlhayxkhzcwcdqdonfmcgsnzmwfcexjdxgzeaxhzeyxlzmykxrqoanwakgpikgazbpcwmjpojgkghmyhawcdlnmxpesokjztkmlmwtqjnmtwtvftelkjyibanawzkyqdevfaayloingxkamjrptnkqdrfepxqcxmudqfdxekjmpcxidnlvkfpxgxolwrhetilmrcpxmngbnhewarsijkseyfzbryfpbnzpyctobzczefxibezcemglpzqitcfgrwawjovikpdlkqgzzaheapurhxygujfxhylomqeheircdqgzyugurmnsdhqrrgaxghtecweklnapjkyqrhtrplepqgjvxzogapcbsmsloqcgsvdjwngdlvnmbpmnqaxzxvyfluyjqorrkfsxmoqstkxloofrwwkexovaktsfrhbhidhuqzmqikbatbunrzlmwmvjtiyyjotacaspmbnnzkdzipuhrptftzcusrekocpjzsabsefdeuimjnaegbtdrecpsktbaylfcogukhwinwrrooijctgucozbzdqtwnjuokdbpxndnunqbbcwfgaj")
+    }.toList()
+    private val events2 = (100_001..200_000).map {
+        Event("862a3cb7-79f7-4f33-93ee-8797bee7e439:3fad95f4-3881-11ec-8760-$it",  "book", "scope","862a3cb7-79f7-4f33-93ee", "862a3cb7-79f7-4f33-93ee-8797bee7e439", false, "name", "type", 1751358236167872, 1751358236167879, "862a3cb7-79f7-4f33-93ee-8797bee7e439:3fad47d2-3881-11ec-8760-e55c32c0bc13", true, setOf(),
+            "xwewsecchmtibgnkiwdfutitniyjltabnpawbkxpuebicuprioefuglxxkrtwjhxgvtadqmpfnxtvegevcyerixjqntqntqqahlytxuaavtinoajlqrejzcvqoswytprimcepebqrbjpabrxqzbwwzlojvlmztexeijyujwkmixqeleyaqniortbwdgfxecpvaqbndajcrjyhrazyojjployqybhrtghimiimpqpplrsmufxicnvfnpanlpntxilchfrezgeqtwqgfisrnxpatjsmvizwrayeapifnabvvejeuqspoifnvmlscapuxzdpccwueptarinrkwnpfgcpxsjhbxkmrvlarepjentaozfdzhjadojdukyphraugginbpqnnwlxooozyxokvnowaoicangcuegvgngiwvzaudlhayxkhzcwcdqdonfmcgsnzmwfcexjdxgzeaxhzeyxlzmykxrqoanwakgpikgazbpcwmjpojgkghmyhawcdlnmxpesokjztkmlmwtqjnmtwtvftelkjyibanawzkyqdevfaayloingxkamjrptnkqdrfepxqcxmudqfdxekjmpcxidnlvkfpxgxolwrhetilmrcpxmngbnhewarsijkseyfzbryfpbnzpyctobzczefxibezcemglpzqitcfgrwawjovikpdlkqgzzaheapurhxygujfxhylomqeheircdqgzyugurmnsdhqrrgaxghtecweklnapjkyqrhtrplepqgjvxzogapcbsmsloqcgsvdjwngdlvnmbpmnqaxzxvyfluyjqorrkfsxmoqstkxloofrwwkexovaktsfrhbhidhuqzmqikbatbunrzlmwmvjtiyyjotacaspmbnnzkdzipuhrptftzcusrekocpjzsabsefdeuimjnaegbtdrecpsktbaylfcogukhwinwrrooijctgucozbzdqtwnjuokdbpxndnunqbbcwfgaj")
     }.toList()
 
     fun transaction() {
         val tx = db.beginStreamTransaction(StreamTransactionOptions().writeCollections(EVENT_COLLECTION, EVENT_EDGES))
         val options = DocumentCreateOptions().streamTransactionId(tx.id)
-        db.collection(EVENT_COLLECTION).insertDocuments(events.subList(1, 100_001), options)
+        db.collection(EVENT_COLLECTION).insertDocuments(events1, options)
         db.commitStreamTransaction(tx.id)
     }
 
     fun f() {
-        db.collection(EVENT_COLLECTION).insertDocuments(events.subList(100_001, 200_001))
+        db.collection(EVENT_COLLECTION).insertDocuments(events2)
     }
 }
